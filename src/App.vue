30e696a621fb5d3b98178ec4100b85d5ab48807b
takeaways <wkdrjsdlf2@gmail.com>
2022-02-03 20:23:35 +0900

    feat: router-link

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link>
-    <router-link to="/login">Login</router-link>
+    <Navbar />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/navbar";
+
 export default {
   name: "app",
+  components: { Navbar },
   data() {
     return {
       msg: "Welcome to Your Vue.js App"
